c11bc7e89f358f58fd6827fa26962b21668202ad
Omar Abdullah Mahran <109427483+Omar-A-Mahran9@users.noreply.github.com>
2024-09-28 16:05:29 +0300

    Resolved some

--- resources/views/web/about.blade.php
+++ resources/views/web/about.blade.php
@@ -1,18 +1,18 @@
 @extends('web.layouts.master')
-@section('title', ' - ' . $page[0]->titleAr)
+@section('title', ' - ' . $page[0]->title)
 
 @section('content')
     <div class="head-page" style="background-image: url({{ asset('images/pages/' . $page[0]->image) }})">
         <div class="container">
-            <h2>{{ $page[0]->titleAr }}</h2>
+            <h2>{{ $page[0]->title }}</h2>
         </div>
     </div>
 
     <nav aria-label="breadcrumb">
         <div class="container">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="{{ url('/') }}">الرئيسية</a></li>
-                <li class="breadcrumb-item active" aria-current="page">من نحن</li>
+                <li class="breadcrumb-item"><a href="{{ url('/') }}">{{ __('HOME') }}</a></li>
+                <li class="breadcrumb-item active" aria-current="page">{{ __('ABOUT SAMA') }}</li>
             </ol>
         </div>
     </nav>
@@ -21,39 +21,24 @@
         <div class="row">
             <div class="col-md-4 col-12" id="presdent_talk">
                 <div class="links">
-
-                    <a href="#presdent_talk">كلمة الرئيس</a>
-                    <a href="#history">تاريخ الشركة</a>
-                    <a href="#value">قيم سما</a>
-                    <a href="#saftey">السلامة أولاً</a>
-                    <a href="#prod">الجودة وزيادة الإنتاجية</a>
-                    <a href="#enviro">البيئة والاستدامة</a>
+                    <a href="#presdent_talk">{{ __('President’s Message') }}</a>
+                    <a href="#history">{{ __('Company History') }}</a>
+                    <a href="#value">{{ __('Values of Sama') }}</a>
+                    <a href="#saftey">{{ __('Safety First') }}</a>
+                    <a href="#prod">{{ __('Quality and Productivity') }}</a>
+                    <a href="#enviro">{{ __('Environment and Sustainability') }}</a>
                 </div>
             </div>
             <div class="col-md-8 col-12">
                 <div id="history" class="inner-content mb-5">
-                    <h3 class="mb-3 fw-bold">كلمة الرئيس</h3>
+                    <h3 class="mb-3 fw-bold">{{ __('President’s Message') }}</h3>
                     <p style="text-align: justify; line-height:32px">
-                        بفضــل وتوفيــق مــن المولــى عــز وجــل خطــت ســما خطــوات متميــزة
-                        طــوال تاريخهــا الممتــد نحــو تحقيــق تجربــة فريــدة ومتميــزة فــي
-                        المجــاالت العســكرية والمدنيــة والمجــاالت األخــرى. نحــن ملتزمــون
-                        بقيــم أساســية تشــكل جوهــر عملنــا، االمتيــاز، النزاهــة، االبتــكار،
-                        السالمـة، والجـودة وسـرعة اإلنتـاج. نعمـل بجـد لتوفيـر حلـول مخصصـة
-                        ومتكاملــة تلبــي احتياجــات كل عميــل، مســتفيدون مــن خبرتنــا الغنيــة
-                        إلنجـاز هـذا الهـدف. نضـع االسـتدامة والتحسـين المسـتمر فـي صـدارة
-                        أولوياتنـا، مـع التركيـز علـى تعزيـز أدائنـا وتقليـل التأثيـر السـلبي علـى
-                        ً
-                        البيئـة. كذلـك نحـن ملتزمـون بتطويـر فريقنـا وتمكينهـم ليكونـوا عنصـرًا
-                        ً فـي رحلتنـا نحـو النجـاح. كفريـق متحـد ومتناغـم، نطمـح
-                        ً ومؤثـرًا
-                        فـاعًال
-                        إلـى تعزيـز مكانتنـا كشـركة رائـدة فـي كافـة مجـاالت عملنـا، مـع الحـرص
-                        علـى تحقيـق أعلـى معاييـر الجـودة والكفـاءة فـي جميـع مشـروعاتنا
-                        وخدماتــنا</p>
+
+                        {{ __('Thanks to the grace of God, Sama has taken distinguished steps throughout its long history towards achieving a unique and distinguished experience in military, civil, and other fields. We are committed to fundamental values that form the essence of our work: excellence, integrity, innovation, safety, quality, and speed of production. We work diligently to provide tailored and integrated solutions that meet the needs of every client, leveraging our rich experience to achieve this goal. We prioritize sustainability and continuous improvement while focusing on enhancing our performance and minimizing negative environmental impact. We are also committed to developing our team and empowering them to be an effective element in our journey towards success. As a united and harmonious team, we aspire to enhance our position as a leading company in all our areas of work,ensuring the achievement of the highest standards of quality and efficiency in all our projects and services.') }}
+                    </p>
                     <div style="text-align:end">
-                        <p class="fw-bold" style="padding-left: 47px">رئيس مجلس االدرة
-                        </p>
-                        <p>سلطان بن سلمان سليم الصاعدي</p>
+                        <p class="fw-bold" style="padding-left: 47px">{{ __('Chairman') }}</p>
+                        <p>{{ __('Sultan bin Salman Saleem Al-Saadi') }}</p>
                     </div>
 
                     <hr class="mob" style="border:2px solid #128977">
@@ -61,197 +46,130 @@
                 </div>
 
                 <div id="value" class="inner-content mb-5">
-                    <h3 class="mb-3 fw-bold">مجموعة ســــــــــــــما (منذ 1990)</h3>
-                    <p style="text-align: justify; line-height:32px">كوننا مجموعة إنشاءات تأسست في عام 1990، فقد شهدنا نموًا
-                        ملحوظًا على مر السنين، وقمنا بتوسيع نطاق أعمالنا وتنويعها في مجموعة متنوعة من القطاعات. ومن خلال
-                        مبادرات النمو الاستراتيجي، تقدم مجموعتنا خدمات في القطاعات المدنية والعمرانية والصناعية والتجارية
-                        والعسكرية والطبية. لدينا تفاني في تحقيق التميز والاستباقية وذلك يتجسد بوضوح في مسيرتنا التطورية.</p>
-
-
+                    <h3 class="mb-3 fw-bold">{{ __('Sama Group (Since 1990)') }}</h3>
+                    <p style="text-align: justify; line-height:32px">
+                        {{ __('As a construction group established in 1990, we have witnessed significant growth over the years, expanding and diversifying our operations across various sectors. Through strategic growth initiatives, our group provides services in civil, structural, industrial, commercial, military, and medical sectors. Our dedication to achieving excellence and proactivity is clearly embodied in our evolutionary journey.') }}
+                    </p>
                 </div>
             </div>
-
-
         </div>
     </div>
+
     <div id="saftey" class="aboutt pb-4 pt-5">
         <div class="container">
             <div class="row align-items-center justify-content-center gap-5">
-
                 <div class="item col-12 col-md-3 mb-4 text-white text-center">
                     <div class="d-flex gap-3 justify-content-center align-items-center">
-
-
-                        <h2 class="fw-bold mt-2 mb-3">
-                            النزاهة
-                        </h2>
-
-
+                        <h2 class="fw-bold mt-2 mb-3">{{ __('Integrity') }}</h2>
                     </div>
                     <div style="margin: 0 auto; text-align: center;">
                         <p class="text-justify" style="display: inline-block; text-align: justify; max-width: 600px;">
-                            نقــوم بجميــع مهامنــا بأمانــة ونزاهــة، لــذا يمكنك أن تثق بنا</p>
+                            {{ __('We conduct all our tasks with honesty and integrity, so you can trust us.') }}
+                        </p>
                     </div>
                 </div>
 
                 <div class="item col-12 col-md-4 mb-4 text-white text-center">
                     <div class="d-flex gap-3 justify-content-center align-items-center">
-
-
-                        <h2 class="fw-bold mt-2  mb-3">
-                            التفرد </h2>
-
-
+                        <h2 class="fw-bold mt-2 mb-3">{{ __('Uniqueness') }}</h2>
                     </div>
                     <div style="margin: 0 auto; text-align: center;">
                         <p class="text-justify" style="display: inline-block; text-align: justify; max-width: 600px;">
-                            كوننا مجموعة تعمل في مجالات متنوعة
-                            نحتاج إلى التميز والابتكار لنكون متقدمين
-                            عن غيرنا بتوفير جميع الحول المتكاملة
-                            وإيجاد الحلول المناسبة التي تلبي احتياجات
-                            .</p>
+                            {{ __("Being a group operating in diverse fields, we need to excel and innovate to stay ahead by providing comprehensive solutions that meet needs.") }}
+                        </p>
                     </div>
                 </div>
 
                 <div class="item col-12 col-md-4 mb-4 text-white text-center">
                     <div class="d-flex gap-3 justify-content-center align-items-center">
-
-
-                        <h2 class="fw-bold mt-2  mb-3">
-                            المضي قدماً </h2>
-
-
+                        <h2 class="fw-bold mt-2 mb-3">{{ __('Moving Forward') }}</h2>
                     </div>
-
-                    <div style="  margin: 0 auto; text-align: center;">
+                    <div style="margin: 0 auto; text-align: center;">
                         <p class="text-justify" style="display: inline-block; text-align: justify; max-width: 600px;">
-                            مؤمنـون بضـرورة التغيير المسـتمر والسـعي
-                            لتحقيـق التحسـين والتعلـم المسـتمر، بهـدف
-                            رـفع المعايـير وتحقـيق المصلـحة المـشتركة</p>
+                            {{ __("We believe in the necessity of continuous change and striving for improvement and continuous learning to elevate standards and achieve mutual benefit.") }}
+                        </p>
                     </div>
                 </div>
-
-
             </div>
         </div>
     </div>
-
 
     <div id="prod" class="container mt-5 mb-5">
         <div class="row">
             <div class="col-12 col-md-6">
-                <h3 class="fw-bold mb-4">السلامة أولاً</h3>
-                <p style="text-align: justify; width:95%">بفضل من الله وامتداداً لتميز سما فقد حققت جائزة افضل مقاول في
-                    مشروع اكساجون
-                    في مدينة نيوم، حيث حصلت على
-                    14 عقد عمل في المشروع، مما يعد إنجازًا مميزاً، حيث لم تحدث أي إصابات بليغة، وهذا يعكس التزامنا المستمر
-                    بالسلامة. لقد اتخذنا تدابير شاملة في ادارة الصحة والسلامة المهنية، لدينا قاعدة عمالية تضم نحو 25,875
-                    شخصًا. قمنا بتنظيم1877 جلسة تعريفية و224 عملية تفتيش. حققنا أكثر من 30,502,451 ساعة عمل آمنة، ما يعكس
-                    التزامنا بتوفير بيئة عمل آمنة. ولم يسجل وقوع أي حوادث كبيرة على مدار الـ14,541,117 كلم التي قمنا
-                    بالقيادة خلالها، مما يؤكد التميز التشغيلي والالتزام بالسلامة في جميع جوانب عملنا.</p>
+                <h3 class="fw-bold mb-4">{{ __('Safety First') }}</h3>
+                <p style="text-align: justify; width:95%">
+                {{ __("By the grace of God and extending Sama’s excellence, we have achieved the best contractor award for the Oxagon project in NEOM, where we secured 14 contracts, marking a significant achievement with no severe injuries, reflecting our ongoing commitment to safety. We have taken comprehensive measures in managing occupational health and safety, with a workforce of about 25,875 people. We organized 1,877 introductory sessions and conducted 224 inspections. We achieved over 30,502,451 safe work hours, reflecting our commitment to providing a safe working environment. No major accidents were recorded over the 14,541,117 km we drove, confirming operational excellence and commitment to safety in all aspects of our work.") }}
+
+                </p>
             </div>
             <div class="col-12 col-md-6">
                 <div class="swiper">
                     <div class="swiper-wrapper">
                         <div class="swiper-slide">
                             <img src="{{ asset('../images/abouts/2.png') }}" alt="">
-
                         </div>
                         <div class="swiper-slide">
                             <img src="{{ asset('../images/abouts/3.png') }}" alt="">
-
                         </div>
                         <div class="swiper-slide">
                             <img src="{{ asset('../images/abouts/1.png') }}" alt="">
-
                         </div>
                     </div>
-
-
                     <div class="swiper-button-prev"></div>
                     <div class="swiper-button-next"></div>
-
                 </div>
             </div>
         </div>
-
     </div>
+
     <hr class="mob" style="border:2px solid #128977">
 
     <div id="enviro" class="container mt-5 mb-5">
         <div class="row">
-            <h3 class="fw-bold mb-4">الجودة وسرعة اإلنتاجية</h3>
+            <h3 class="fw-bold mb-4">{{ __('Quality and Productivity') }}</h3>
 
             <div class="col-12 col-md-6">
-                <h5 style="color: #128977">الجودة</h5>
-                <p style="text-align: justify; width:95%">نحـن ملتزمـون بتحقيـق جـودة تتجـاوز المعاييـر فـي جميـع مجـاالت
-                    عملنـا. نهـدف إلـى تحقيـق مسـتوى اسـتثنائي مـن التفـوق فـي القطاعـات
-                    ً لـسرعة التـسليم
-                    ً ا ـمن التصامـيم المبتـكرة إـلى التنفـيذ الدقـيق وـصوًال
-                    الـتي نعـمل بـها وفـخورون بجمـيع مـشاريعنا، ـبدًء</p>
+                <h5 style="color: #128977">{{ __('Quality') }}</h5>
+                <p style="text-align: justify; width:95%">
+                {{ __("We are committed to achieving quality that exceeds standards in all our fields of work. We aim to achieve an exceptional level of excellence, from innovative designs to precise execution, reaching the delivery of the projects we take pride in.") }}                                                                                                                                                                                                                             
+                </p>
             </div>
             <div class="col-12 col-md-6">
-                <h5 style="color: #128977">سرعة االنتاجية</h5>
-                <p style="text-align: justify; width:95%">مفتـاح نجـاح سـما ليـس اختصـار األعمـال، بـل إنشـاء الظـروف
-                    المالئمـة إلكمـال الجـداول الزمنيـة والخطـط فـي الوقـت المناسـب واالسـتفادة
-                    القصـوى مـن الوقـت المتـاح. يتضمـن ذلـك التخطيـط المسـبق وتوفيـر العمـال المدربيـن واألدوات والمعـدات
-                    المناسـبة، باإلضافـة إلـى توفيـر
-                    التعليــمات واإلــشراف المناــسب للعــمال</p>
+                <h5 style="color: #128977">{{ __('Productivity Speed') }}</h5>
+                <p style="text-align: justify; width:95%">
+                   {{ __("The key to Sama’s success is not shortcutting work, but creating the right conditions to complete timelines and plans in a timely manner, making the utmost of the available time. This includes prior planning, providing trained workers and appropriate tools and equipment, in addition to providing suitable instructions and proper supervision for the workers.") }}</p>
             </div>
-
         </div>
         <div class="row mt-5 m-auto">
-            <div class="col-6 col-md-3 2-"> <img src="{{ asset('../images/abouts/logo1.png') }}" alt=""
-                    class="sup">
-            </div>
-            <div class="col-6 col-md-3"> <img src="{{ asset('../images/abouts/logo2.png') }}" alt="" class="sup">
-            </div>
+            <div class="col-6 col-md-3"> <img src="{{ asset('../images/abouts/logo1.png') }}" alt=""
+                    class="sup"></div>
+            <div class="col-6 col-md-3"> <img src="{{ asset('../images/abouts/logo2.png') }}" alt=""
+                    class="sup"></div>
             <div class="col-6 col-md-3"> <img src="{{ asset('../images/abouts/logo3.png') }}" alt=""
-                    class="sup">
-            </div>
+                    class="sup"></div>
             <div class="col-6 col-md-3"> <img src="{{ asset('../images/abouts/logo4.png') }}" alt=""
-                    class="sup">
-            </div>
+                    class="sup"></div>
         </div>
     </div>
 
     <div style="background-color: #128977" class="p-4">
         <div class="container mt-5 mb-5 text-white">
             <div class="row justify-content-center">
-                <h3 class="fw-bold mb-4 text-center">البيئة والاستدامة</h3>
-                <h5 style="color: #fffff; margin-top:-13px; font-size:15px" class="text-center fw-bold mb-4">
-                    الاستدامة ليست مجرد
-                    جزء من اسرتاتيجيتنا،
-                    بل هي هويتنا،
-                    ملتزمون بكوننا جزءًاً
-                    من الحل يف مواجهة
-                    التحديات البيئية.</h5>
+                <h3 class="fw-bold mb-4 text-center">{{ __('Environment and Sustainability') }}</h3>
+                <h5 style="color: #ffffff; margin-top:-13px; font-size:15px" class="text-center fw-bold mb-4">
+                    {{ __('Sustainability is not just a part of our strategy; it is our identity. We are committed to being part of the solution in facing environmental challenges.') }}
+                </h5>
                 <div class="mb-4" style="border-bottom:5px solid #666666; width:100px; margin:auto; margin-top:-10px;">
                 </div>
                 <div class="container">
                     <div class="col-12 col-md-8 m-auto">
-                        <p class="text-center" style="line-height: 33px;">نــدرك يف سام أن مســتقبل األعامل
-                            ال يكمــن فقــط يف النجــاح املادي،
-                            بــل يف الحفــاظ عىل كوكبنــا، لــذا
-                            نســعى لتطبيــق معــايري البنــاء
-                            األخرض كاســتخدام مــواد مســتدامة
-                            وتصمــيامت تعتمــد عىل كفــاءة
-                            اســتخدام الطاقــة، نركــز عىل ان
-                            تكــون أعاملنــا متميــزة بقلــة تأثريهــا
-                            بدعــم
-                            عىل البيئــة، تلتــزم سام أيضــًاً
-                            مصـادر الطاقـة املتجـددة واالسـتثامر
-                            يف تقنيــات الطاقــة الشمســية،
-                            نطبــق أفضــل املامرســات الصناعيــة
-                            لـضامن عمليـات إنتاجيـة تـحرتم البيئـة،
-                            وتســهم يف الحفــاظ عليهــا.
+                        <p class="text-center" style="line-height: 33px;">
+                            {{ __('At Sama, we recognize that the future of business is not just about financial success, but also about preserving our planet. Therefore, we strive to implement green building standards by using sustainable materials and designs that emphasize energy efficiency. We focus on ensuring that our operations have minimal environmental impact. We are also committed to renewable energy sources and investing in solar energy technologies. We apply the best industrial practices to ensure production processes respect the environment and contribute to its preservation.') }}
                         </p>
                     </div>
                 </div>
             </div>
-
-
-
         </div>
     </div>
 
